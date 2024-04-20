b92e679260ad19f7068755ce33b5b7a8aa71dbeb
unknown <jborelli95@gmail.com>
2024-04-20 14:49:01 -0300

    Navbar responsive

---
+++ src/app/components/home/home-season/home-season.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-season',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-season.component.html',
+  styleUrl: './home-season.component.css'
+})
+export class HomeSeasonComponent {
+
+}
--- src/app/views/navigation/home-view/home-view.component.ts
+++ src/app/views/navigation/home-view/home-view.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../../components/shared/navbar/navbar.component';
+import { HeroComponent } from '../../../components/home/hero/hero.component';
+import { HomeNewsComponent } from '../../../components/home/home-news/home-news.component';
+import { HomeSeasonComponent } from '../../../components/home/home-season/home-season.component';
 
 @Component({
   selector: 'app-home-view',
   standalone: true,
   imports: [
-    NavbarComponent
+    NavbarComponent,
+    HeroComponent,
+    HomeNewsComponent,
+    HomeSeasonComponent
   ],
   templateUrl: './home-view.component.html',
   styleUrl: './home-view.component.css'
